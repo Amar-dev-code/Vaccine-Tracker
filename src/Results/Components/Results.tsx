0906a53b36ee191d53cda76f9f88a94c7927341e
amar_singh_1102@outlook.com <amar_singh_1102@outlook.com>
2021-05-27 23:48:37 +0530

    Making website responsive.

--- src/Results/Components/Results.tsx
+++ src/Results/Components/Results.tsx
@@ -26,52 +26,58 @@ function Component(props: Props) {
   if (data.length > 0) {
     display = (
       <>
-        <div className="wrapper">
-          <TableContainer>
-            <Table aria-label="simple table">
-              <TableHead>
-                <TableCell>Vaccination Center</TableCell>
-                <TableCell>Minimum Age Limit</TableCell>
-                <TableCell align="center">Date</TableCell>
-                <TableCell>Available Slots</TableCell>
-                <TableCell>Vaccine Type</TableCell>
-                <TableCell>Dose1 Slots</TableCell>
-                <TableCell>Dose2 Slots</TableCell>
-              </TableHead>
-              <TableBody>
-                {data.map((element: any) =>
-                  element.sessions.map((data: any) => {
-                    return (
-                      <TableRow>
-                        <TableCell className="sticky-col">
-                          {element.name}
-                        </TableCell>
-                        <TableCell align="center">
-                          {data.min_age_limit}
-                        </TableCell>
-                        <TableCell>{data.date}</TableCell>
-                        <TableCell align="center">
-                          {data.available_capacity}
-                        </TableCell>
-                        <TableCell>{data.vaccine}</TableCell>
-                        <TableCell align="center">
-                          {data.available_capacity_dose1}
-                        </TableCell>
-                        <TableCell align="center">
-                          {data.available_capacity_dose2}
-                        </TableCell>
-                      </TableRow>
-                    );
-                  })
-                )}
-              </TableBody>
-            </Table>
-          </TableContainer>
+        <div className="viewScreen">
+          <div className="wrapper">
+            <TableContainer>
+              <Table aria-label="simple table">
+                <TableHead>
+                  <TableCell className="sticky-col1">
+                    Vaccination Center
+                  </TableCell>
+                  <TableCell className="sticky-col2">
+                    Minimum Age Limit
+                  </TableCell>
+                  <TableCell align="left">Date</TableCell>
+                  <TableCell>Available Slots</TableCell>
+                  <TableCell>Vaccine Type</TableCell>
+                  <TableCell>Dose1 Slots</TableCell>
+                  <TableCell>Dose2 Slots</TableCell>
+                </TableHead>
+                <TableBody>
+                  {data.map((element: any) =>
+                    element.sessions.map((data: any) => {
+                      return (
+                        <TableRow>
+                          <TableCell className="sticky-col first-col">
+                            {element.name}
+                          </TableCell>
+                          <TableCell className="sticky-col second-col">
+                            {data.min_age_limit}
+                          </TableCell>
+                          <TableCell align="center">{data.date}</TableCell>
+                          <TableCell align="center">
+                            {data.available_capacity}
+                          </TableCell>
+                          <TableCell>{data.vaccine}</TableCell>
+                          <TableCell align="center">
+                            {data.available_capacity_dose1}
+                          </TableCell>
+                          <TableCell align="center">
+                            {data.available_capacity_dose2}
+                          </TableCell>
+                        </TableRow>
+                      );
+                    })
+                  )}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </div>
         </div>
         <Box marginTop="5px" className="flex-container">
           <a href="https://www.cowin.gov.in/home" target="blank">
             <Typography variant="overline">
-              * Please click to book your Covid Vaccine.
+              * Book your Covid Vaccine.
             </Typography>
           </a>
         </Box>
